067fbd89e238b4e28db3b2a851f45a197940d4f1
another-desiree <desiree.clever@gmail.com>
2024-08-30 16:58:29 +0200

    feat: new social card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'B    I    P',
   tagline: 'biust   insight   project',
-  favicon: '/img/logo.png',
+  favicon: '/img/social-card.png',
 
   // Set the production url of your site here
   url: 'https://halo-saber.github.io',
