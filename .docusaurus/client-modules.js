87bcf02c3d2dc0f66d1f33c1d24b9657215311b4
another-desiree <desiree.clever@gmail.com>
2024-09-13 23:59:33 +0200

    clean

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("C:\\Users\\cd23018473\\Downloads\\my-website\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\cd23018473\\Downloads\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\cd23018473\\Downloads\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\cd23018473\\Downloads\\my-website\\src\\css\\custom.css"),
-];
