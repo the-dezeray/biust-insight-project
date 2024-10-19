b1e933d816559863eb3efae214ef29455b137b44
the-dezeray <chingwaru.desiree@gmail.com>
2024-10-19 17:40:01 +0200

    feat: force signout (cookie related)

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -3,14 +3,17 @@ import { auth, signOut, getUserStatusFromCookie } from './firebase';
 
 const shouldLogoutUser = () => {
   const userStatus = getUserStatusFromCookie();
-  if (!userStatus) return false;
+  if (!userStatus) {
+    console.log("User status cookie not found, logging out");
+    return true;
+  }
 
   const { isPayable, createdAt } = userStatus;
   const creationDate = new Date(createdAt);
   const currentDate = new Date();
   const daysSinceCreation = Math.floor((currentDate - creationDate) / (1000 * 60 * 60 * 24));
 
-  return !isPayable && daysSinceCreation >= 3;
+  return !isPayable && daysSinceCreation >= 2;
 };
 
 export default function Root({ children }) {
@@ -18,8 +21,8 @@ export default function Root({ children }) {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       if (user) {
         if (shouldLogoutUser()) {
-          console.log("User is not payable and trial period has ended, logging out");
-          signOut(auth).catch(error => console.error("Error logging out:", error));
+          console.log("User should be logged out, signing out");
+          signOut(auth).catch(error => console.error("Error signing out:", error));
         }
       }
     });
